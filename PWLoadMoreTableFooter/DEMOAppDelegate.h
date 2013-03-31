176f8c2faea2be76cee65466f69be73259025092
Puttin Wong <yzwang.nj@gmail.com>
2013-03-31 17:05:01 +0800

    Initial Commit

---
+++ PWLoadMoreTableFooter/DEMOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DEMOAppDelegate.h
+//  PWLoadMoreTableFooter
+//
+//  Created by Puttin Wong on 3/31/13.
+//  Copyright (c) 2013 Puttin Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DEMOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PWLoadMoreTableFooter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PWLoadMoreTableFooter
+//
+//  Created by Puttin Wong on 3/31/13.
+//  Copyright (c) 2013 Puttin Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DEMOAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DEMOAppDelegate class]));
+    }
+}
